ff623f39aec730bd3e0df390b83fda43bfb3d405
iElectro <ejose.acc@gmail.com>
2022-02-04 18:24:34 -0500

    Cambios en el css

--- src/js/script.js
+++ src/js/script.js
@@ -47,12 +47,23 @@ const tracker1 = document.querySelector('.tracker-number1'),
     tracker4 = document.querySelector('.tracker-number4')
     flagSection = document.querySelector('.flag-section')
 
+
+//Tabs tracker4
+
+const topCasesContainer = document.getElementById('top-cases'),
+    todayCasesContainer = document.getElementById('today-cases'),
+    topDeathsContainer = document.getElementById('top-deaths'),
+    todayDeathsContainer = document.getElementById('today-deaths'),
+    topActiveContainer = document.getElementById('top-active'),
+    topRecoveredContainer = document.getElementById('top-recovered')
+
 //Tracker pages
 const trackerPages = document.querySelectorAll('.tracker-page')
 
 const trackerPage2 = document.getElementById('tracker-page2'),
     trackerPage3 = document.getElementById('tracker-page3'),
-    trackerPage4 = document.getElementById('tracker-page4')
+    trackerPage4 = document.getElementById('tracker-page4'),
+    trackerPage5 = document.getElementById('tracker-page5')
 
 //Icons
 const icons = document.querySelectorAll('.icons')
@@ -60,13 +71,30 @@ const iconsSvg = document.querySelectorAll('.icons-svg')
 
 const tracker2Icon = document.getElementById('tracker2-icon'),
 tracker3Icon = document.getElementById('tracker3-icon'),
-tracker4Icon = document.getElementById('tracker4-icon')
+tracker4Icon = document.getElementById('tracker4-icon'),
+tracker5Icon = document.getElementById('tracker5-icon')
 
+
+//Table
+const tBody = document.getElementById('table-body')
 
 /*-----------Components----------*/
 //Option
 const option = (country) => {
     let element = `<option id=${country.toLowerCase()}>${country}</option>`
+    return element
+}
+
+const topTenTab = (flag, country, number)=>{
+    let element = `
+    <div class="card">
+    <div class="info-country">
+
+    <picture class="foto"><img src=${flag} alt=""></picture>
+    <span>${country}</span>
+    </div>
+    <span>${number}</span>
+    </div>`
     return element
 }
 
@@ -90,12 +118,38 @@ const topTenItem = (flag, country, cases)=> {
     return element
 }
 
+//Table Row
+const tableR = (flag, country, cases, newCases, deaths, newDeaths, recovered, active, critical, tested) => {
+    let element = `
+    <tr>
+        <td><img class="table-img" src=${flag} alt=""></td>
+        <td>${country}</td>
+        <td>${cases}</td>
+        <td>${newCases}</td>
+        <td>${deaths}</td>
+        <td>${newDeaths}</td>
+        <td>${recovered}</td>
+        <td>${active}</td>
+        <td>${critical}4</td>
+        <td>${tested}</td>
+    </tr>`
+    return element
+}
+
 
 /*----------This functions handles the initial DOM load and sets all the data--------*/
 const domPaint = async () => {
     let data = await getData('https://disease.sh/v3/covid-19/all')
     let countries = await getData('https://disease.sh/v3/covid-19/countries')
     let topTen = await getMany('https://disease.sh/v3/covid-19/countries?sort=cases', 10)
+    let topToday = await getMany('https://disease.sh/v3/covid-19/countries?sort=todayCases', 10)
+    let topDeaths = await getMany('https://disease.sh/v3/covid-19/countries?sort=deaths', 10)
+    let topTodayDeaths = await getMany('https://disease.sh/v3/covid-19/countries?sort=todayDeaths', 10)
+    let topActive = await getMany('https://disease.sh/v3/covid-19/countries?sort=active', 10)
+    let topRecovered = await getMany('https://disease.sh/v3/covid-19/countries?sort=recovered', 10)
+
+
+
 
 
     //Navbar 
@@ -104,7 +158,6 @@ const domPaint = async () => {
         icon.addEventListener('click', ()=>{
             removeActive(iconsSvg)
             icon.childNodes[1].classList.add('active')
-
         })
     })
 
@@ -116,6 +169,37 @@ const domPaint = async () => {
     topTen.forEach(element => {
         addValue(flagSection, topTenItem(element.countryInfo.flag, element.country, element.cases))
     })
+
+    //Set top 10 in tracker 4
+
+    topTen.forEach(element => {
+        addValue(topCasesContainer, topTenTab(element.countryInfo.flag, element.country, element.cases))
+    })
+    topToday.forEach(element => {
+        addValue(todayCasesContainer, topTenTab(element.countryInfo.flag, element.country, element.todayCases))
+    })
+    topDeaths.forEach(element => {
+        addValue(topDeathsContainer, topTenTab(element.countryInfo.flag, element.country, element.deaths))
+    })
+    topTodayDeaths.forEach(element => {
+        addValue(todayDeathsContainer, topTenTab(element.countryInfo.flag, element.country, element.todayDeaths))
+    })
+    topRecovered.forEach(element => {
+        addValue(topRecoveredContainer, topTenTab(element.countryInfo.flag, element.country, element.recovered))
+    })
+    topActive.forEach(element => {
+        addValue(topActiveContainer, topTenTab(element.countryInfo.flag, element.country, element.active))
+    })
+
+
+    //Set table
+    countries.forEach(country => {
+        addValue(tBody, tableR(country.countryInfo.flag, country.country, country.cases, country.todayCases, country.deaths, country.todayDeaths, country.recovered, country.active, country.critical, country.tests))
+
+    })
+    $(document).ready( function () {
+        $('#table').DataTable();
+    } );
 
 
     //Set main trackers
@@ -164,6 +248,12 @@ window.addEventListener('DOMContentLoaded', ()=> {
             tracker.classList.add('inactive')
         })
         trackerPage4.classList.remove('inactive')
+    })
+    tracker5Icon.addEventListener('click', ()=> {
+        trackerPages.forEach(tracker => {
+            tracker.classList.add('inactive')
+        })
+        trackerPage5.classList.remove('inactive')
     })
     domPaint()
 })
